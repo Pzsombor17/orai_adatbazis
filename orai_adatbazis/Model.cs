564b9a974d18fb09b86e75dee20142b6a61a5f1a
ParryHotter <Pap.Zsombor@C19-01>
2026-05-12 08:31:30 +0200

    Add project files.

---
+++ orai_adatbazis/Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai_adatbazis
+{
+    public class Muzeum
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Price { get; set; }
+        public double Rating { get; set; }
+        public string Type { get; set; }
+
+    }
+    public class Model
+    {
+        public List<Muzeum> muzeums = new();
+        public Model() 
+        {
+            Import();
+        }
+        private void Import()
+        {
+            muzeums = File.ReadAllLines("muzeumok.txt").Skip(1)
+                .Select(x=> x.Split(';')).Select(x=> new Muzeum 
+                {
+                    Name = x[0],
+                    City = x[1],
+                    Address = x[2],
+                    Price = Convert.ToInt32 (x[3]),
+                    Rating = Convert.ToDouble (x[4]),
+                    Type = x[5]
+                }).ToList();
+        }
+    }
+}
---
+++ orai_adatbazis/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace orai_adatbazis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Model model = new();
+            Console.WriteLine(model.muzeums.Count);
+        }
+    }
+}
