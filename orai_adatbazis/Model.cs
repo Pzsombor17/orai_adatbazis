33e2a0a7c862e55f7fa67500f7cbe36d019f089a
ParryHotter <Pap.Zsombor@C19-01>
2026-05-12 09:44:52 +0200

    sd

--- orai_adatbazis/Model.cs
+++ orai_adatbazis/Model.cs
@@ -40,6 +40,74 @@ namespace orai_adatbazis
         {
             return muzeums.Where(x => x.Name[0]==betu).Select(x => x.Name).ToList();
         }
+        public string LowestRating()
+        {
+            return muzeums.OrderBy(x => x.Rating).Select(x=>x.Name).First();
+        }
+        public double HighestRating(string type)
+        {
+            return muzeums.Where(x => x.Type == type).Max(x => x.Rating);
+        }
+        public double AvgRating()
+        {
+            return muzeums.Average(x => x.Rating);
+        }
+        public int CountCity(string city)
+        {
+            return muzeums.Where(x => x.City == city).Count();
+        }
+        public List<string> EqualRating(double rating)
+        {
+            return muzeums.Where(x => x.Rating == rating).Select(x => x.Name).ToList();
+        }
+        public int CountRating()
+        {
+            return muzeums.Where(x => x.Rating > 3.6).Count();
+        }
+        public List<string> Idontknow(string type, double maxrating)
+        {
+            return muzeums.Where(x => x.Type == type && x.Rating < maxrating).Select(x => x.Name).ToList();
+        }
+        public List<string> MuseumType(string citynamepart)
+        {
+            return muzeums.Where(x => x.City.Contains(citynamepart)).Select(x => x.Name).ToList();
+        }
+        public int MinRating(double minrating)
+        {
+            return muzeums.Where(x => x.Rating > minrating).Count();
+        }
+        public string MuseumType2(string name)
+        {
+            return muzeums.Where(x => x.Name == name).Select(x => x.Type).First();
+        }
+        public List<string> BetweenRating(double minrating, double maxrating)
+        {
+            return muzeums.Where(x => x.Rating >= minrating && x.Rating <= maxrating).Select(x => x.Name).ToList();
+        }
+        public List<string> MuseumType3(string type)
+        {
+            return muzeums.Where(x => x.Type == type).Select(x => x.Name).ToList();
+        }
+        public List<string> NameLength(int minnamelength)
+        {
+            return muzeums.Where(x => x.Name.Length > minnamelength).Select(x => x.Name).ToList();
+        }
+        public List<string> WordCount(int number)
+        {
+            return  muzeums.Where(x=> x.Name.Split(' ').Length == number).Select(x => x.Name).ToList();
+        }
+        public List<string> WordCount2(string type,double minrating)
+        {
+            return muzeums.Where(x => x.Type == type && x.Name.Split(' ').Length > 2 && x.Rating > minrating).Select(x => x.Name).ToList();
+        }
+        public List<string> AvgMuseumRating(string cityname)
+        {
+            double avg = muzeums.Where(x => x.City == cityname).Average(x => x.Rating);
+            return muzeums.Where(x => x.City == cityname && x.Rating > avg).Select(x => x.Name).ToList();
+        }
+
+        
+        
 
     }
 }
--- orai_adatbazis/Program.cs
+++ orai_adatbazis/Program.cs
@@ -8,6 +8,13 @@
             Model model = new();
             Console.WriteLine(model.muzeums.Count);
             model.BeginChar('I').ForEach(x => Console.WriteLine(x));
+            Console.WriteLine(model.LowestRating());
+            Console.WriteLine(model.HighestRating("fény"));
+            Console.WriteLine(model.AvgRating());
+            Console.WriteLine(model.CountCity("Budapest"));
+            model.EqualRating(4.5).ForEach(x => Console.WriteLine(x));
+            model.Idontknow("fény", 4.5).ForEach(x => Console.WriteLine(x));
+            model.BetweenRating(3.5,4.0).ForEach(x => Console.WriteLine(x));
         }
     }
 }
